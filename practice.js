ca3537ea514c45c4cc217e32cea1c9d41fcf803f
yunyeook <dudhr17050@naver.com>
2024-09-25 18:39:31 +0900

    arc(x, y, 반지름, 시작각도, 종료각도,그리는 방향)
    시작각도와 종료각도 : 0 ~2.0 *Math.PI
    그리는방향 : 기본값 false로 시계방향(설정하지않으면 기본값으로 자동 설정.)

--- practice.js
+++ practice.js
@@ -19,6 +19,7 @@ ctx.fill(); //-> 색칠도 하기
 ctx.rect(500, 600, 50, 50); //-> fill()메서드 이후에 나온건 색칠이든 윤곽선이든 안됨! 채우려면 ctx.fill() 메서드 다시 호출
 ctx.rect(500, 700, 50, 50);
 
+//별만들기
 ctx.beginPath();
 ctx.moveTo(50, 400); //펜을 설정 위치로 옮기기 => 옮겨진 위치가 현재위치가 됨.
 ctx.lineTo(200, 400); // 현재 위치에서 설정 위치까지 선그리기 => 옮겨진 위치가 새로운 현재위치가 됨.
@@ -29,3 +30,20 @@ ctx.lineTo(50, 400);
 ctx.stroke();
 ctx.fillStyle = 'yellow';
 ctx.fill(); //하위경로의 시작지점(moveTo) 부터 마지막 지점(마지막lineTo)까지 가상으로 연결되어 색칠됨!!
+
+//사람 만들기
+ctx.beginPath();
+ctx.fillStyle = 'bisque';
+ctx.fillRect(500, 150, 15, 100);
+ctx.fillRect(600, 150, 15, 100);
+ctx.fillRect(530, 150, 55, 180);
+//arc(x, y, 반지름, 시작각도, 종료각도, 그리는방향) : 그리는방향 시계방향false가 기본값
+// ctx.arc(557.5, 90, 50, 0, 0.5 * Math.PI, true);
+ctx.arc(557.5, 90, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = 'black';
+ctx.arc(540, 75, 10, Math.PI, 2 * Math.PI);
+ctx.arc(575, 75, 10, Math.PI, 2 * Math.PI);
+ctx.fill();
