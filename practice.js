dbe7e3804ba7afd591d7c249c922de52914249a3
yunyeook <dudhr17050@naver.com>
2024-09-25 17:48:17 +0900

    canvas
    그래픽이나 애니메이션을 그릴 수 있는 요소
    
    .getContext("2d") : 2차원 그래픽을 그림.
    
    Context2D의 메서드
    fillRect(x, y, width, height) : 직사각형그리고 색채우기
    stokeRect(x, y, width, height) : 직사각형 그리고 윤곽선 그리기
    .rect(x, y ) : 직사각형 모양만 설정하기
    .fill()
    .stoke()
    
    .beginPath : 새로운 경로 생성하기
    .moveTo(x, y) : 새로운 하위경로 시작지점
    .lineTo(x, y) : 새로운 하위경로의 마지막지점

---
+++ practice.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d'); //대분자 아니고 소문자
+
+//css에서 정한 canvas의 크기를 js파일에게도 알려줘야한다.
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(50, 50, 100, 200); //색이 채워진 직사각형그리기
+ctx.strokeRect(200, 50, 100, 200); //선으로 직사각형 그리기
+
+ctx.rect(500, 400, 50, 50);
+ctx.stroke(); //-> 윤곽선만 그리기
+
+ctx.beginPath(); //-> 새로운 경로 만들기(해당 메서드가 없으면 같은 경로라서 위의것도 아래의 fill의 영향으로 색이 칠해진다)
+ctx.rect(500, 500, 50, 50);
+ctx.fillStyle = 'red';
+ctx.fill(); //-> 색칠도 하기
+
+ctx.rect(500, 600, 50, 50); //-> fill()메서드 이후에 나온건 색칠이든 윤곽선이든 안됨! 채우려면 ctx.fill() 메서드 다시 호출
+ctx.rect(500, 700, 50, 50);
+
+ctx.beginPath();
+ctx.moveTo(50, 400); //펜을 설정 위치로 옮기기 => 옮겨진 위치가 현재위치가 됨.
+ctx.lineTo(200, 400); // 현재 위치에서 설정 위치까지 선그리기 => 옮겨진 위치가 새로운 현재위치가 됨.
+ctx.lineTo(75, 500);
+ctx.lineTo(125, 350);
+ctx.lineTo(175, 500);
+ctx.lineTo(50, 400);
+ctx.stroke();
+ctx.fillStyle = 'yellow';
+ctx.fill(); //하위경로의 시작지점(moveTo) 부터 마지막 지점(마지막lineTo)까지 가상으로 연결되어 색칠됨!!
