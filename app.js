c6c9d72782439c77d6151fbe886737566a093017
yunyeook <dudhr17050@naver.com>
2024-09-26 13:45:49 +0900

    마우스 누르고 움직일때마다 그리기
    mouse이벤트
    mousedown : 누른 상태
    mouseup: 뗀 상태
    click : 누르고 떼었을때 발생
    mouseleave : 해당 요소 밖으로 마우스가 이동할때

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ const ctx = canvas.getContext('2d'); //대분자 아니고 소문자
 //css에서 정한 canvas의 크기를 js파일에게도 알려줘야한다.
 canvas.width = 800;
 canvas.height = 800;
-const colors = ['#F7A3A7', '#F7AD97', '#FAD89E', '#C8D7C4', '#BBCBD2', '#B7B6D6', '#E2BBD8'];
-
 ctx.lineWidth = 2;
+let isPainting = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    // console.log(event.offsetX, event.offsetY);
+    return; //반환해야 움직이는 순간순간마다 그려지니까!!
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function onMoveDown() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', onMoveDown);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
 
 // canvas.addEventListener('click', onClick);
-canvas.addEventListener('mousemove', onClick);
-
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
-}
+// function onClick(event) {
+//   console.log(event.offsetX, event.offsetY);
+// }
