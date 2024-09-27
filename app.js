82b305d6c123495265a883e39b6acd5d0aefff44
yunyeook <dudhr17050@naver.com>
2024-09-27 21:15:16 +0900

    이미지업로드, 텍스트넣기, 캔버스저장하기
    이미지파일업로드
    1. input type file로 이미지 브라우저 메모리에 업로드
    2. 브라우저 메모리에 업로드된 이미지의 url주소 저장 : URL.createObjectURL
    3. 이미지 태그 생성 후 src에 url주소 wjwkd
    4. 파일 업로드 될 때 이미지 넣기 : ctx.drawImage(이미지태그,x,y,width,height)
    
    텍스트넣기
    텍스트 먼저 입력 후 canvas 더블클릭하면 도장마냥 텍스트 찍히도록함.
    
    캔버스그림 저장하기
    1. 캔버스 그림의 데이터를 URL로 저장하기
    2. a 태그 생성 후 href에 URL저장
    3. a 태그의 download 속성 넣기
    4. a태그 자동 클릭되도록 click()넣기

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const color = document.querySelector('#color');
 const modeButton = document.getElementById('mode-button');
 const removeAllButton = document.getElementById('removeAll-button');
 const eraseButton = document.getElementById('erase-button');
+const fileInput = document.getElementById('file');
+const textInput = document.getElementById('text');
+const saveButton = document.getElementById('save-button');
 
 //Prototype이 HTMLCollection이라 forEach메서드 사용못하므로 배열로 만든다
 const colorOptions = Array.from(document.getElementsByClassName('color-option'));
@@ -13,6 +16,7 @@ const colorOptions = Array.from(document.getElementsByClassName('color-option'))
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = 'round';
 let isPainting = false;
 let isFilling = false;
 
@@ -28,6 +32,10 @@ function onMove(event) {
 }
 function onMoveDown() {
   isPainting = true;
+  //배경색채우기
+  if (isFilling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 function cancelPainting() {
   isPainting = false;
@@ -59,6 +67,7 @@ function colorClick(event) {
 
   color.value = event.target.dataset.color; //클릭한 컬러가 무엇인지 알기위해 input color의 색을 바꿔주기
 }
+//각 색상마다  이벤트리스너넣기!
 colorOptions.forEach(color => color.addEventListener('click', colorClick));
 
 //========================= 배경채우기 =========================//
@@ -71,13 +80,14 @@ function modeChange() {
     modeButton.innerText = 'Draw';
   }
 }
-function fillBackground() {
-  if (isFilling) {
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
-}
 modeButton.addEventListener('click', modeChange);
-canvas.addEventListener('click', fillBackground);
+
+// function fillBackground() {
+//   if (isFilling) {
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+//   }
+// }
+// canvas.addEventListener('click', fillBackground); ==> 캔버스에 마우스 누르기만해도 배경비뀌게하면되니까 mousedown이벤트발생시 추가함.
 
 //========================= 지우기 =========================//
 function removeAll() {
@@ -92,3 +102,42 @@ function erase() {
 }
 removeAllButton.addEventListener('click', removeAll);
 eraseButton.addEventListener('click', erase);
+
+//========================= 이미지파일 업로드하기 =========================//
+function onFileChange(event) {
+  const file = event.target.files[0]; // input의 file타입은 multiple이 가능하므로 여러개의 file을 가진 files 배열이다.
+
+  const url = URL.createObjectURL(file); // blob:http://~~  blob부터 주소복사하면 이미지 뜸.
+
+  const image = new Image(); //== html파일안에 <img src=''/>와 같음
+
+  image.src = url; //
+
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, 200, 200);
+    fileInput.value = null; //fileInput의 value가 이미지 파일 주소이므로 새 이미지 업로드 위해 null로 설정함.
+  };
+}
+fileInput.addEventListener('change', onFileChange);
+
+//========================= 텍스트 넣기 =========================//
+function onDoubleClick(event) {
+  const text = textInput.value;
+  ctx.save();
+  ctx.lineWidth = 1;
+  ctx.font = '40px serif';
+  ctx.fillText(text, event.offsetX, event.offsetY);
+  // ctx.strokeText(text, event.offsetX, event.offsetY);
+  ctx.restore();
+}
+canvas.addEventListener('dblclick', onDoubleClick);
+
+//========================= 캠버스의 그림 저장하기 =========================//
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement('a');
+  a.href = url; //그림의 url로 설정
+  a.download = 'myDrawing.jpg'; //어떻게 저장할지 설정
+  a.click(); //다운로드하기
+}
+saveButton.addEventListener('click', onSaveClick);
